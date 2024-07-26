7fd07c0f90c22571fdbe7392215d3d2f7c510de1
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-07-26 21:37:07 +0300

    feat: added profile picture

--- src/components/header/AnimatedLogo.tsx
+++ src/components/header/AnimatedLogo.tsx
@@ -25,7 +25,7 @@ function Logo() {
   return (
     <a className="block" href="/" title="Nav to home">
       <img
-        className="size-[40px] select-none object-cover rounded-2xl"
+        className="size-12 select-none object-cover rounded-full"
         src={author.avatar}
         alt="Site owner avatar"
       />
